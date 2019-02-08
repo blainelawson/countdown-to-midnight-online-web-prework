40544b082d95970aaa2485735a5fb92dc886ae67
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-08 15:57:04 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,9 @@
 #write your code here
 
-def countdown(num_of_times)
-  while num_of_times > 0
-  
+def countdown(number)
+  while num_of_times >= 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  puts "HAPPY NEW YEAR!"
 end
