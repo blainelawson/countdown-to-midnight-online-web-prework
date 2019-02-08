415648ef647082e2d2c0da5e091fa89a8cf5284e
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-08 16:09:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,20 @@
 #write your code here
 
 def countdown(number)
+  
   while number >= 0
+  
     puts "#{number} SECOND(S)!"
     number -= 1
     
     def countdown_with_sleep(seconds)
+      
       sleep seconds
+      
     end
+    
   end
+  
   "HAPPY NEW YEAR!"
+  
 end
