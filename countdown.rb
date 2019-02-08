e41c69a2119a988cca401dda21ae505699b36c9f
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-08 16:26:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,18 +1,18 @@
 #write your code here
 
 def countdown(number)
+  
   seconds = 1
+  def countdown_with_sleep(seconds)
+    sleep seconds
+        end
   
   while number >= 0
   
     puts "#{number} SECOND(S)!"
     number -= 1
     
-    def countdown_with_sleep(seconds)
-      
-      sleep seconds
-      
-    end
+
     
     countdown_with_sleep(seconds)
     
