a878224265e5356a18d75159bc1f3c1fc2de397b
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-08 16:31:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,12 +8,10 @@ def countdown(number)
   end
   
   while number >= 0
-  
     puts "#{number} SECOND(S)!"
     number -= 1
     
     countdown_with_sleep(seconds)
-    
   end
   
   "HAPPY NEW YEAR!"
